405997b71513bb2649b8c999be424dd424be2239
dkorolev <korwork87@yahoo.com>
2015-09-24 14:27:54 +0300

    added Add new comment functionality (gui + controller)

--- src/main/java/controller/CommentsController.java
+++ src/main/java/controller/CommentsController.java
@@ -3,14 +3,15 @@ package controller;
 import dao.CommentRepository;
 import dao.UserRepository;
 import model.Comment;
+import model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Collection;
+import java.util.Optional;
 
 /**
  * Additional controller to operate with comments.
@@ -28,11 +29,29 @@ public class CommentsController {
     CommentRepository commentRepository;
 
 
+    //get all comments for user
     @RequestMapping(method = RequestMethod.GET)
     public Collection<Comment> getComments(@PathVariable String username) {
         return commentRepository.findByUserUsername(username);
     }
-//    public String getComments(@PathVariable String username) {
-//        return "Hello, "+username;
-//    }
+
+    //add new comment for user
+    @RequestMapping(method = RequestMethod.POST)
+    public void createComment(@PathVariable String username, @Valid @RequestBody Comment comment) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException(username));
+        comment.setUser(user);
+        commentRepository.save(comment);
+
+    }
+
+
+
+    //exception if user was not found
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    private class UserNotFoundException extends RuntimeException {
+        public UserNotFoundException(String username) {
+            super("could not find user '" + username + "'.");
+        }
+    }
 }
--- src/main/resources/public/comments.js
+++ src/main/resources/public/comments.js
@@ -1,13 +1,17 @@
+//create module
 var module = angular.module('Blog', ['ngResource']);
 
+//create factory to be able to create Commen object + user resource url
 module.factory('Comment', function($resource) {
     return $resource(':username/comments', { username: '@username' });
 })
+    //create controller which is used in index.html
     .controller('CommentsController', function($scope, Comment) {
         var url = function() {
             return {username:$scope.username||'dkorolev'};
         }
 
+        //update existing comments
         var update = function() {
             console.log(url());
             $scope.comments = Comment.query(url());
@@ -15,6 +19,19 @@ module.factory('Comment', function($resource) {
 
         $scope.update = update;
 
+
+        // add new comment function (save and clear textarea)
+        $scope.add = function() {
+            var comment = new Comment();
+            comment.text = $scope.text;
+            comment.$save(url(), function() {
+                $scope.text = "";
+                update();
+            })
+        }
+
+
+        //use update by default (when page first loading)
         update();
 
 });
